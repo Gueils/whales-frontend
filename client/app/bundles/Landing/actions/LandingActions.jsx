3183a73d5fc798cfbdd6074ad0f2d4bb7232b268
Luis Ignacio <cejas@icalialabs.com>
2017-03-25 22:34:14 -0600

    New face
    Facelifting is not something that I like. Besides, isn't weird that
    women are the ones who beautify themselves for males? In Nature, it is
    the opposite. How fucked this civilization is?? Anyway, but for this
    occasion, a facelifting was more than necessary. This is an almost done
    design.

--- client/app/bundles/Landing/actions/LandingActions.jsx
+++ client/app/bundles/Landing/actions/LandingActions.jsx
@@ -1,3 +1,5 @@
+/* global WorkspaceController, document */
+
 import {
   RECEIVE_DOCKERFILE,
   RECEIVE_DOCKERCOMPOSE } from '../constants/LandingConstants';
@@ -13,11 +15,21 @@ export const receiveDockercompose = dockercompose => ({
   dockercompose,
 });
 
+export const updateFilesContainer = (dockerfile, dockercompose) => {
+  const prefixLoadingAnimation = document.getElementsByClassName('prefix-loading-animation')[0];
+  prefixLoadingAnimation.innerHTML = "You're welcome!";
+
+  const loadingAnimation = document.getElementsByClassName('loading-animation')[0];
+  loadingAnimation.innerHTML = ' :)';
+  WorkspaceController.hideRestOfTheLanding().showFiles(dockerfile, dockercompose);
+};
+
 export const analizeRepository = repository => (dispatch) => {
   return RepositoryApi.analize(repository)
     .then((response) => {
       dispatch(receiveDockerfile(response.data.dockerfile));
       dispatch(receiveDockercompose(response.data.dockercompose));
+      updateFilesContainer(response.data.dockerfile, response.data.dockercompose);
     })
     .catch((error) => {
       console.log(error);
